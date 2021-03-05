bf326638c86cc856b0c83a4c286a5507938e876c
cpettet <cpettet11@gmail.com>
2021-03-05 13:00:33 -0800

    Adding X's right now

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,28 +5,38 @@
 //
 // when something is clicked
 //  - if child === null
-//  - add the graphic
+//  - add the graphic, player-x.svg & player-o.svg
 //  - increment the counter
 //  -
 
 let counter = 0;
-const squares = document.querySelectorAll("#tic-tac-toe-board div")
-squares.forEach(square => {
-  square.addEventListener('click', event => {
+const squares = document.querySelectorAll("#tic-tac-toe-board div");
+squares.forEach((square) => {
+  square.addEventListener("click", (event) => {
+    console.log(event.target);
     if (!square.hasChildNodes()) {
       //square is a div
-
-
+      setImage(event.target);
     }
-  })
-})
+  }, {once: true});
+});
 
-function setImage() {
-  if ()
-    //set image based on counter conditional.
+function setImage(square) {
+  const imgDiv = document.createElement('div');
+  const imgX = '<img src="player-x.svg">';
+  const imgO = '<img src="player-o.svg">';
+
+  if (counter % 2 === 0) {
+    // even
+    imgDiv.innerHTML = imgX;
+  } else {
+    // odd
+    imgDiv.innerHTML = imgO;
+  }
+
+  square.append(imgDiv);
 }
 
-
-let dogContainer = document.getElementById('dog-img-container')
-let img = '<img src="./dog.png" class = "dog-img" />'
-dogContainer.innerHTML = img;
+// let dogContainer = document.getElementById("dog-img-container");
+// let img = '<img src="./dog.png" class = "dog-img" />';
+// dogContainer.innerHTML = img;
