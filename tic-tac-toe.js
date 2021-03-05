00be180b67950156a5e535ecce81a1745a26e6c1
Geoffrey <geoffreyyang@gmail.com>
2021-03-05 16:37:11 -0500

    check winner progress

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -13,30 +13,64 @@ let counter = 0;
 const squares = document.querySelectorAll("#tic-tac-toe-board div");
 squares.forEach((square) => {
   square.addEventListener("click", (event) => {
-    console.log(event.target);
-    if (!square.hasChildNodes()) {
-      //square is a div
-      setImage(event.target);
-    }
-  }, {once: true});
+
+    setImage(event.target);
+
+  }, {once:true});
 });
 
 function setImage(square) {
   const imgDiv = document.createElement('div');
-  const imgX = '<img src="player-x.svg">';
-  const imgO = '<img src="player-o.svg">';
+  const imgX = '<img src="player-x.svg" class="xImg">';
+  const imgO = '<img src="player-o.svg" class="oImg">';
 
   if (counter % 2 === 0) {
     // even
     imgDiv.innerHTML = imgX;
+    square.classList.add("x")
   } else {
     // odd
     imgDiv.innerHTML = imgO;
+    square.classList.add("o")
   }
 
   square.append(imgDiv);
+  counter++;
+  checkWinner();
 }
 
-// let dogContainer = document.getElementById("dog-img-container");
-// let img = '<img src="./dog.png" class = "dog-img" />';
-// dogContainer.innerHTML = img;
+function checkWinner() {
+// create a nodelist (qselector) for each row + each col 6 nodelists +2 diagnols
+  // iterate through each nodelist
+
+  let row1 = document.querySelectorAll(".row-1")
+  let row2 = document.querySelectorAll(".row-2")
+  let row3 = document.querySelectorAll(".row-3")
+  let col1 = document.querySelectorAll(".col-1")
+  let col2 = document.querySelectorAll(".col-2")
+  let col3 = document.querySelectorAll(".col-3")
+  // let forwardDiagnol
+  // checkThree(row1)
+}
+
+function checkThree(values) {
+  const xOrO = values[0].classList.contains("x")
+  let xChecker = [];
+  let oChecker = [];
+  values.forEach(square => {
+    if (square.classList.contains('x')) {
+      xChecker.push(true);
+    }
+  });
+
+  values.forEach(square => {
+    if (square.classList.contains('o')) {
+      oChecker.push(true);
+    }
+  });
+
+  if (xChecker.every(true)) return "x";
+  if (oChecker.every(true)) return "o";
+
+}
+
